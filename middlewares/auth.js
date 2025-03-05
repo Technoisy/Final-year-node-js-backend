5cd2d05a396f8b0349d6da6902e20f0a9f5298ef
irfanmit <faisalirfan2502@gmail.com>
2025-03-05 11:30:16 +0530

    L

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,28 +3,28 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     const authHeader = req.get('authorization');
     if(!authHeader){
-        console.log('authHeader : ' + authHeader)
+        // console.log('authHeader : ' + authHeader)
         req.isAuth = false;
         return next();
     }
-    console.log('authHeader : ' + authHeader)
+    // console.log('authHeader : ' + authHeader)
     const token = authHeader.split(' ')[1];
-    console.log('token : '+ token)
+    // console.log('token : '+ token)
     let decodedToken;
     try{
         decodedToken = jwt.verify(token, 'somesupersecretsecret');  
-       console.log('decodedToken')
+    //    console.log('decodedToken')
     } catch(err){
-        console.log('catch errror')
+        // console.log('catch errror')
         req.isAuth = false;
         return next();
     }
     if(!decodedToken){
-        console.log('No decodedtoken')
+        // console.log('No decodedtoken')
         req.isAuth = false;
         return next();
     }
-    console.log('ending')
+    // console.log('ending')
     req.userId = decodedToken.userId;
     req.isAuth = true;
     next();
